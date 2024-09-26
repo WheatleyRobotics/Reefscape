8aab0613967ddc02d1f8e790ea6251ede1b275f1
Kouyang07 <kaicheng@kouyang.dev>
2024-09-26 14:50:52 -0400

    Init with AK

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Reefscape";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2024-09-26 14:31:08 EDT";
+  public static final long BUILD_UNIX_TIME = 1727375468104L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}

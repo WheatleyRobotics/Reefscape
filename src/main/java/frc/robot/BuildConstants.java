5b4a728ff6154282a202a7473d223ea5a4c3b160
Kouyang07 <kaicheng@kouyang.dev>
2024-10-15 22:17:55 -0400

    Woah Choreo with PathPlanner working

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "a0a44993bb2c3cf8a992d365c38b69d65480e0ab";
-  public static final String GIT_DATE = "2024-10-14 15:54:31 EDT";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "fb932b8f891ba3b74e94e18ecc95e88bf120d4b3";
+  public static final String GIT_DATE = "2024-10-14 22:44:32 EDT";
   public static final String GIT_BRANCH = "choreo";
-  public static final String BUILD_DATE = "2024-10-14 22:43:28 EDT";
-  public static final long BUILD_UNIX_TIME = 1728960208364L;
+  public static final String BUILD_DATE = "2024-10-15 22:17:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1729045023155L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

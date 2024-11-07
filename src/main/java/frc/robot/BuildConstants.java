b868dbe92e469437bb42e85100c0858bc7b30bdb
Kouyang07 <kaicheng@kouyang.dev>
2024-11-07 11:27:57 -0500

    Chore: Cleaned up unnecessary files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,11 +6,11 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "RobotCode2024";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "417e701afdec66e8118d83419fefdd3b6db44846";
-  public static final String GIT_DATE = "2024-11-07 11:13:32 EST";
+  public static final String GIT_SHA = "fd3e077f6b0347ea21ffda55115fc88a36077f50";
+  public static final String GIT_DATE = "2024-11-07 11:17:44 EST";
   public static final String GIT_BRANCH = "test/clean-akit";
-  public static final String BUILD_DATE = "2024-11-07 11:17:19 EST";
-  public static final long BUILD_UNIX_TIME = 1730996239380L;
+  public static final String BUILD_DATE = "2024-11-07 11:27:43 EST";
+  public static final long BUILD_UNIX_TIME = 1730996863520L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

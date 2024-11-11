6af6f690f2153f6693b20bde1c913161e7c81dc0
Brandon Jiang <brandonjiang@brandons-air.mynetworksettings.com>
2024-11-10 21:41:50 -0500

    feat: added leftCamera and rightCamera to the instance field

--- src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
+++ src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
@@ -1,13 +1,17 @@
 package frc.robot.subsystems.vision;
 
 import frc.lib.structs.VisionPose;
+import org.photonvision.PhotonCamera;
 
 public class VisionIOSim implements VisionIO {
+
+  private PhotonCamera leftCam = new PhotonCamera("leftCamera");
+  private PhotonCamera rightCam = new PhotonCamera("rightCamera");
+
   @Override
   public void updateIOInputs(VisionIOInputs inputs) {}
 
   @Override
   public VisionPose getVisionPose() {
     return new VisionPose();
-  }
-}
+  }}

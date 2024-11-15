a08a9a24af5178ab41fab8f67d9c0806b78236a1
Kouyang07 <kaicheng@kouyang.dev>
2024-11-15 14:18:22 -0500

    Silenced Joystick warning & Removed test control

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "f1bad2da1626ce547e4912907886c0daffd97839";
-  public static final String GIT_DATE = "2024-10-09 15:41:05 EDT";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "3a37e2f6a0e4d58e2d23c431875704ef341d3df3";
+  public static final String GIT_DATE = "2024-11-13 16:36:52 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-11-13 13:48:02 EST";
-  public static final long BUILD_UNIX_TIME = 1731523682621L;
+  public static final String BUILD_DATE = "2024-11-15 14:06:56 EST";
+  public static final long BUILD_UNIX_TIME = 1731697616703L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

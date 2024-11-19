8a7cfa4a66c66e3a5e9d6b0f7f1ec7327324d82d
Kouyang07 <kaicheng@kouyang.dev>
2024-11-19 13:46:44 -0500

    Working GC System

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "3a37e2f6a0e4d58e2d23c431875704ef341d3df3";
-  public static final String GIT_DATE = "2024-11-13 16:36:52 EST";
-  public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-11-15 14:06:56 EST";
-  public static final long BUILD_UNIX_TIME = 1731697616703L;
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "a08a9a24af5178ab41fab8f67d9c0806b78236a1";
+  public static final String GIT_DATE = "2024-11-15 14:18:22 EST";
+  public static final String GIT_BRANCH = "feat/visualVM";
+  public static final String BUILD_DATE = "2024-11-19 13:43:58 EST";
+  public static final long BUILD_UNIX_TIME = 1732041838747L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

08707032db69455ca19aa64b499aaafb09f08b6e
Kouyang07 <kaicheng@kouyang.dev>
2024-12-09 11:51:20 -0500

    Updated memory setting

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "96a19887dfa0dd7a8f491b3bfdca394481474bbc";
-  public static final String GIT_DATE = "2024-12-06 12:34:17 EST";
-  public static final String GIT_BRANCH = "feat/setpoint-generator";
-  public static final String BUILD_DATE = "2024-12-07 22:36:22 EST";
-  public static final long BUILD_UNIX_TIME = 1733628982002L;
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "77704333cea7cbf4cac61ed7d7639f21863c2b05";
+  public static final String GIT_DATE = "2024-12-07 22:43:57 EST";
+  public static final String GIT_BRANCH = "feat/2025";
+  public static final String BUILD_DATE = "2024-12-09 11:48:20 EST";
+  public static final long BUILD_UNIX_TIME = 1733762900255L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

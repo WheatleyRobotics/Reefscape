8f652d9bcbd32e9a202780ec70ef9b5de2d78031
Kouyang07 <kaicheng@kouyang.dev>
2025-01-06 13:15:53 -0500

    Updated to latest PP

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 33;
-  public static final String GIT_SHA = "57ef90c888e0b542a25279bbe2dd5d18c0d0d715";
-  public static final String GIT_DATE = "2025-01-04 13:53:04 EST";
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "70ff7d318ecea9895f765aeb78b917383d6e93eb";
+  public static final String GIT_DATE = "2025-01-04 22:24:33 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-01-04 15:54:21 EST";
-  public static final long BUILD_UNIX_TIME = 1736024061500L;
+  public static final String BUILD_DATE = "2025-01-06 13:10:41 EST";
+  public static final long BUILD_UNIX_TIME = 1736187041269L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

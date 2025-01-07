9baeac27f3cfbf378aaf8c0e008333ebb5f7dcff
Kouyang07 <kaicheng@kouyang.dev>
2025-01-06 22:54:45 -0500

    Updated wpilib to kickoff versio

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 35;
-  public static final String GIT_SHA = "8f652d9bcbd32e9a202780ec70ef9b5de2d78031";
-  public static final String GIT_DATE = "2025-01-06 13:15:53 EST";
-  public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-01-06 21:33:20 EST";
-  public static final long BUILD_UNIX_TIME = 1736217200666L;
+  public static final int GIT_REVISION = 37;
+  public static final String GIT_SHA = "e64b1de75cf05695b11ff436d85e71ec15bbf1a2";
+  public static final String GIT_DATE = "2025-01-06 23:00:10 EST";
+  public static final String GIT_BRANCH = "feat/kickoff-wpilib";
+  public static final String BUILD_DATE = "2025-01-06 23:16:20 EST";
+  public static final long BUILD_UNIX_TIME = 1736223380550L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

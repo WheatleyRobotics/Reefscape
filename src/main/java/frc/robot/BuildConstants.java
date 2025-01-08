4239e0eb4eca1f221ff7fa10af71e765f46c4f57
Shawn Khillan <55472239+Sparten2360@users.noreply.github.com>
2025-01-08 17:38:01 -0500

    GC updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "da2183338e99e4a722c7350dbfb8e2c9c5bd5aff";
-  public static final String GIT_DATE = "2025-01-07 15:58:44 EST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "d4192d7ac0038c4860c8375e6670021868317791";
+  public static final String GIT_DATE = "2025-01-08 17:29:42 EST";
   public static final String GIT_BRANCH = "feat/kickoff-wpilib";
-  public static final String BUILD_DATE = "2025-01-08 17:18:45 EST";
-  public static final long BUILD_UNIX_TIME = 1736374725335L;
+  public static final String BUILD_DATE = "2025-01-08 17:37:36 EST";
+  public static final long BUILD_UNIX_TIME = 1736375856869L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

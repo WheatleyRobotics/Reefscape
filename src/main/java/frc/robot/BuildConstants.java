4ff7165591e3742bf69ddaa83fe53a1cc39851cf
Kouyang07 <kaicheng@kouyang.dev>
2025-01-14 12:59:43 -0500

    Added DriveController

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 49;
-  public static final String GIT_SHA = "8a06a35f46f27e5d9c3e80d05f9753e464ef9de3";
-  public static final String GIT_DATE = "2025-01-14 11:11:47 EST";
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "8f6a2704a4d8e77fa229514e8a1a6d17e9550370";
+  public static final String GIT_DATE = "2025-01-14 13:08:00 EST";
   public static final String GIT_BRANCH = "feat/vision";
-  public static final String BUILD_DATE = "2025-01-14 11:43:03 EST";
-  public static final long BUILD_UNIX_TIME = 1736872983936L;
+  public static final String BUILD_DATE = "2025-01-14 15:02:42 EST";
+  public static final long BUILD_UNIX_TIME = 1736884962752L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

8e2431b31699d08b39ab2dcaf4437147ca78e22e
Kouyang07 <kaicheng@kouyang.dev>
2025-01-18 13:09:02 -0500

    Added PathPlanner paths

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "b4f6f9c85339f3f10ca1a56bd117c9d916c4dd7c";
-  public static final String GIT_DATE = "2025-01-14 16:27:10 EST";
-  public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-01-16 04:27:22 EST";
-  public static final long BUILD_UNIX_TIME = 1737019642801L;
+  public static final int GIT_REVISION = 54;
+  public static final String GIT_SHA = "d3cf514ebdd4c511e9b9a3b518ad14f7bba7ffbb";
+  public static final String GIT_DATE = "2025-01-16 04:29:03 EST";
+  public static final String GIT_BRANCH = "feat/superstructure";
+  public static final String BUILD_DATE = "2025-01-18 13:07:02 EST";
+  public static final long BUILD_UNIX_TIME = 1737223622337L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,12 +25,8 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.GyroIO;
-import frc.robot.subsystems.drive.GyroIOPigeon2;
-import frc.robot.subsystems.drive.ModuleIO;
-import frc.robot.subsystems.drive.ModuleIOSim;
-import frc.robot.subsystems.drive.ModuleIOSpark;
+import frc.robot.commands.DriveController;
+import frc.robot.subsystems.drive.*;
 import frc.robot.subsystems.vision.*;
 import frc.robot.util.RobotState;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
@@ -166,6 +162,10 @@ public class RobotContainer {
                     },
                     drive)
                 .ignoringDisable(true));
+
+    controller
+        .y()
+        .onTrue(new DriveController(drive, new Pose2d(3.7, 3, Rotation2d.fromDegrees(40))));
   }
 
   /**

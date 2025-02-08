7402e3dd4712ea6eace1485f9eef80232a905667
Kouyang07 <kaicheng@kouyang.dev>
2025-02-08 12:00:41 -0500

    Fixed vision

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 70;
-  public static final String GIT_SHA = "969fb9f7f91bb81a2ab581377e00cb86289a5f9c";
-  public static final String GIT_DATE = "2025-02-04 19:05:25 EST";
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "9d7ef25e0abe029302b81b99f72f8e01e6a1b8d6";
+  public static final String GIT_DATE = "2025-02-04 19:43:55 EST";
   public static final String GIT_BRANCH = "feat/driver-assist";
-  public static final String BUILD_DATE = "2025-02-04 19:42:12 EST";
-  public static final long BUILD_UNIX_TIME = 1738716132519L;
+  public static final String BUILD_DATE = "2025-02-08 11:58:56 EST";
+  public static final long BUILD_UNIX_TIME = 1739033936762L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

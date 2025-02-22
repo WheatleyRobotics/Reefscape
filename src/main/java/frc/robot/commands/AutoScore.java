6fa819daf63fed606927c77ecbd5c3e5e098f02f
Shawn Khillan <55472239+Sparten2360@users.noreply.github.com>
2025-02-21 19:25:47 -0500

    All Choreo Permutations + centered Clear Reef
    I added every single choreo path needed for every single permutation of autos and I fixed the clear reef command to center the robot after scoring This means all the choreo paths should run where they start it's 5 am im going to bed lol

--- src/main/java/frc/robot/commands/AutoScore.java
+++ src/main/java/frc/robot/commands/AutoScore.java
@@ -1,5 +1,8 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -40,15 +43,24 @@ public class AutoScore {
                 FieldConstants.addOffset(
                     FieldConstants.getBranch(RobotState.getInstance().getCurrentZone(), right),
                     -0.4)),
-        superstructure.runGoal(() -> state.get().getEject()).withTimeout(0.5),
-        getClearReef(drive),
-        superstructure.runGoal(SuperstructureState.STOW).until(superstructure::atGoal));
+        superstructure.runGoal(() -> state.get().getEject()).withTimeout(0.25),
+        getClearReef(drive)
+            .deadlineFor(
+                superstructure
+                    .runGoal(SuperstructureState.STOW)
+                    .until(superstructure::atGoal))); //
   }
 
   public static Command getClearReef(Drive drive) {
     return Commands.sequence(
-        Commands.run(() -> drive.runVelocity(new ChassisSpeeds(-2.5, 0, 0)))
-            .until(() -> RobotState.getInstance().isClearedReef()),
+        new DriveToPose(
+            drive,
+            () ->
+                FieldConstants.addOffset(
+                    FieldConstants.Reef.centerFaces[
+                        RobotState.getInstance().getCurrentZone().getFace()].plus(new Transform2d(new Translation2d(),Rotation2d.fromRadians(Math.PI)))
+                        ,
+                    -1)),
         Commands.runOnce(() -> drive.runVelocity(new ChassisSpeeds(0, 0, 0))));
   }
 }

e72462a92b4046edf94c8d6e906862bd566d76d8
Shawn Khillan <55472239+Sparten2360@users.noreply.github.com>
2025-03-04 18:18:47 -0500

    Making sure robot type is correct and updated so robot type is only used for PID switch cases

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 151;
-  public static final String GIT_SHA = "3d064853a3d55c1419bad2ee46c1c3fb893dc9e0";
-  public static final String GIT_DATE = "2025-03-04 02:47:49 EST";
+  public static final int GIT_REVISION = 152;
+  public static final String GIT_SHA = "e4a6c69f70af7e6110c637f768d59df5afc4d146";
+  public static final String GIT_DATE = "2025-03-04 18:07:29 EST";
   public static final String GIT_BRANCH = "AutoTuningMasterMerge";
-  public static final String BUILD_DATE = "2025-03-04 18:00:32 EST";
-  public static final long BUILD_UNIX_TIME = 1741129232784L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-04 18:14:05 EST";
+  public static final long BUILD_UNIX_TIME = 1741130045120L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }

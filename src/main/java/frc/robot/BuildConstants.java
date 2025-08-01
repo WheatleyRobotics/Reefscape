a221e6efa9d94d2cb32973d295723768a4ef5af3
Kouyang07 <kaicheng@kouyang.dev>
2025-08-01 00:09:20 -0400

    Driving auto & updated vendordep

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 189;
-  public static final String GIT_SHA = "5dcca8354fc94014dea220b455dd0f622d04e162";
-  public static final String GIT_DATE = "2025-03-30 19:20:34 EDT";
-  public static final String GIT_BRANCH = "feat/algae-align";
-  public static final String BUILD_DATE = "2025-04-04 16:45:25 EDT";
-  public static final long BUILD_UNIX_TIME = 1743799525269L;
+  public static final int GIT_REVISION = 190;
+  public static final String GIT_SHA = "79a82f7fc7fa0705f3e620bc4e4c522163aa876f";
+  public static final String GIT_DATE = "2025-04-06 13:39:13 EDT";
+  public static final String GIT_BRANCH = "static-choreo";
+  public static final String BUILD_DATE = "2025-08-01 00:04:19 EDT";
+  public static final long BUILD_UNIX_TIME = 1754021059596L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ import lombok.Getter;
 public final class Constants {
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : Mode.SIM;
   public static final double loopPeriodSecs = 0.02;
-  @Getter private static RobotType robotType = RobotType.COMPBOT;
+  @Getter private static RobotType robotType = RobotType.SIMBOT;
   public static final boolean tuningMode = true;
   public static final boolean autoIntake = false;
 
